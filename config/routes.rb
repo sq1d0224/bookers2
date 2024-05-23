3fd7769e2d51af022b4e290836ccb295e55c3c13
sq1d0224 <isho05010224@gmail.com>
2024-05-23 11:34:17 +0000

    [Add]header,footer

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   
   root to: "homes#top"
   devise_for :users
+  get 'homes/about', to: 'homes#about', as: :about
   
   resources :books, only: [:new, :index, :show]
   resources :users, only: [:show, :edit]
