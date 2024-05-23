aa259b5865c27e649d33ef30dac618298b595147
sq1d0224 <isho05010224@gmail.com>
2024-05-23 10:14:46 +0000

    [Add]ファイルの設定

--- app/models/book.rb
+++ app/models/book.rb
@@ -2,4 +2,6 @@ class Book < ApplicationRecord
   
   has_one_attached :image
   
+  belongs_to :user
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,10 @@
 Rails.application.routes.draw do
-  get 'users/show'
-  get 'users/edit'
-  root to: "homes#top"
-  get 'books/new'
-  get 'books/index'
-  get 'books/show'
   
+  root to: "homes#top"
   devise_for :users
+  
+  resources :books, only: [:new, :index, :show]
+  resources :users, only: [:show, :edit]
+  
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
