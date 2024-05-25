ab3704c5e47c88da2d6668a84654c3fd0992e17d
sq1d0224 <isho05010224@gmail.com>
2024-05-25 07:51:13 +0000

    [Add]bookのindexページ作成

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   devise_for :users
   get 'homes/about', to: 'homes#about', as: :about
   
-  resources :books, only: [:new, :index, :show]
+  resources :books, only: [:new, :create, :index, :show]
   resources :users, only: [:index, :show, :edit]
   
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
