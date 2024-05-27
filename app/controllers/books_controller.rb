886fcb6849efaa7b263789299ada29d709e94506
sq1d0224 <isho05010224@gmail.com>
2024-05-27 10:32:19 +0000

    [Add]自己紹介欄追加

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -12,9 +12,11 @@ class BooksController < ApplicationController
 
   def index
     @books = Book.all
+    @book = Book.new
   end
 
   def show
+    @book = Book.find(params[:id])
   end
   
   private
