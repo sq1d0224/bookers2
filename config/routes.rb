9b2205e4021e173a8a5a1cca1ee00a3f3c9b0343
sq1d0224 <isho05010224@gmail.com>
2024-05-28 09:19:52 +0000

    [Add]update機能追加

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   get 'homes/about', to: 'homes#about', as: :about
   
   resources :books, only: [:new, :create, :index, :show]
-  resources :users, only: [:index, :show, :edit]
+  resources :users, only: [:index, :show, :edit, :update]
   
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
