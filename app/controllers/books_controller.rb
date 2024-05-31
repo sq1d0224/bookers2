04593ca5af5dd147d845e2f5d136578911898f14
sq1d0224 <isho05010224@gmail.com>
2024-05-31 09:27:08 +0000

    [Add]フラッシュメッセージの実装

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -2,12 +2,14 @@ class BooksController < ApplicationController
   def new
     @book = Book.new
   end
-  
+
   def create
     @book = Book.new(book_params)
     @book.user_id = current_user.id
-    @book.save
+    if @book.save
+      flash[:notice] = "You have created book successfully."
     redirect_to book_path(@book)
+    end
   end
 
   def index
@@ -21,25 +23,27 @@ class BooksController < ApplicationController
     @user = @book.user
     @new_book = Book.new
   end
-  
+
   def edit
     @book = Book.find(params[:id])
   end
-  
+
   def update
     @book = Book.find(params[:id])
-    @book.update(book_params)
+    if @book.update(book_params)
+      flash[:notice] = "You have updated book successfully."
     redirect_to book_path(@book)
+    end
   end
-  
+
   def destroy
     book = Book.find(params[:id])
     book.destroy
     redirect_to books_path
   end
-  
+
   private
-  
+
   def book_params
     params.require(:book).permit(:title, :body)
   end
