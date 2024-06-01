7016e9bd82f0182532dad9830883c608447978d7
sq1d0224 <isho05010224@gmail.com>
2024-06-01 10:11:44 +0000

    [修正]

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,6 +9,10 @@ class ApplicationController < ActionController::Base
   def after_sign_out_path_for(resource)
     root_path
   end
+  
+  def after_sign_up_path_for(resource)
+    user_path(current_user.id)
+  end
 
   protected
 
